52d3f3b0e4d9591dc6d2c3580c06ad0f94121fa5
Igor <siafin2010@gmail.com>
2019-10-11 19:12:57 +0300

    God bless me

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  include Auth	
+  protect_from_forgery with: :exception
+  before_action :get_current_user
+
+
+  def destroy
+    respond_to do |format|
+      format.html { redirect_to root_url, notice: 'Not implemented yet' }
+      format.json { head :no_content }
+    end
+  end  
+
 end
