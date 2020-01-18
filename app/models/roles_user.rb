7ab020149c22bea24271f6d2249d2fd612c573f0
Igor <siafin2010@gmail.com>
2020-01-18 15:01:37 +0200

    a

--- app/models/roles_user.rb
+++ app/models/roles_user.rb
@@ -2,7 +2,7 @@ class RolesUser < ApplicationRecord
   belongs_to :role
   belongs_to :user
 
-	# default_scope { where("deleted_at IS NULL")}
+	default_scope { where("deleted_at IS NULL")}
 
   validates :user_id, uniqueness: { scope: [:role_id, :deleted_at] }
 
