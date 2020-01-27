106cba689872513eb5eb1a8efcaa3abc3f6d8e8e
Igor <siafin2010@gmail.com>
2020-01-27 23:32:17 +0200

    a

--- app/javascript/packs/App/components/newDocForm.jsx
+++ app/javascript/packs/App/components/newDocForm.jsx
@@ -1,35 +1,48 @@
 import React from 'react'
-import { Formik } from 'formik'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
 import classes from './newDocForm.module.css'
+import Loader from '../UI/loader'
 
 export default function NewDocForm(props){
 
-
+	function validateName1(value) {
+	  let error;
+	  if (!value) {
+	    error = 'Required';
+	  } else if (value.length && value.length>10) {
+	    error = 'too long (need to be less than 10)';
+	  }
+	  return error;
+	}
 
 	return(
 	  <div>
 	    <Formik
-	      initialValues={{ name: 'jared' }}
+	      initialValues={{ name1: 'jared' }}
 	      onSubmit={(values, actions) => {
-	        setTimeout(() => {
-	          console.log(values, actions)
-	          actions.setSubmitting(false);
-	        }, 1000);
-	      }}
+		      	setTimeout(()=> {
+		      		alert('submitted! '+ values.name1 )
+		      		actions.setSubmitting(false)
+		          console.log(values, actions)
+		      	}, 3000 )
+	        }
+	      }
 	    >
-	      {props => (
-	        <form onSubmit={props.handleSubmit}>
+	      { ({isSubmitting}) => (
+	        <Form>
 	        	<label htmlFor="name1">input1</label>
-	          <input
+	          <Field
 	            type="text"
-	            onChange={props.handleChange}
-	            onBlur={props.handleBlur}
-	            value={props.values.name}
 	            name="name1"
+	            validate={validateName1} 
+	            disabled={isSubmitting}
 	          />
-	          {props.errors.name && <div id="feedback">{props.errors.name}</div>}
-	          <button type="submit">Submit</button>
-	        </form>
+	          <ErrorMessage name='name1'>
+	          	{msg => <div>{'ERROR: ' + msg}</div>}
+	          </ErrorMessage>
+	          <button type="submit" disabled={isSubmitting}>Submit</button>
+	          <Loader isLoading={isSubmitting}/>
+	        </Form>
 	      )}
 	    </Formik>
 	  </div>
