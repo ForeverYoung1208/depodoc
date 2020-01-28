50e969f4d3b9378935247d6b71b7adb036b10327
Igor <siafin2010@gmail.com>
2020-01-28 23:14:44 +0200

    a

--- app/javascript/packs/App/components/newDocForm.jsx
+++ app/javascript/packs/App/components/newDocForm.jsx
@@ -18,28 +18,63 @@ export default function NewDocForm(props){
 	return(
 	  <div>
 	    <Formik
-	      initialValues={{ name1: 'jared' }}
+	      initialValues={{ 
+	      	doctype:'',
+	      	name: '',
+	      	face:'',
+	      	note:''
+	      	 }}
 	      onSubmit={(values, actions) => {
 		      	setTimeout(()=> {
-		      		alert('submitted! '+ values.name1 )
+		      		alert('submitted! '+ values.name )
 		      		actions.setSubmitting(false)
 		          console.log(values, actions)
-		      	}, 3000 )
+		      	}, 2000 )
 	        }
 	      }
 	    >
 	      { ({isSubmitting}) => (
 	        <Form>
-	        	<label htmlFor="name1">input1</label>
+
+	        	<label htmlFor="doctype">Тип докуента</label>
 	          <Field
 	            type="text"
-	            name="name1"
+	            name="doctype"
 	            validate={validateName1} 
 	            disabled={isSubmitting}
 	          />
-	          <ErrorMessage name='name1'>
-	          	{msg => <div>{'ERROR: ' + msg}</div>}
-	          </ErrorMessage>
+	          <ErrorMessage name='doctype'>	{msg => <div>{'ERROR: ' + msg}</div>} </ErrorMessage>
+
+	        	<label htmlFor="name">Назва документа</label>
+	          <Field
+	            type="text"
+	            name="name"
+	            validate={validateName1} 
+	            disabled={isSubmitting}
+	          />
+	          <ErrorMessage name='name'>	{msg => <div>{'ERROR: ' + msg}</div>} </ErrorMessage>
+
+	        	<label htmlFor="face">Належить особі</label>
+	          <Field
+	            type="text"
+	            name="face"
+	            validate={validateName1} 
+	            disabled={isSubmitting}
+	          />
+	          <ErrorMessage name='face'>	{msg => <div>{'ERROR: ' + msg}</div>} </ErrorMessage>
+
+	        	<label htmlFor="note">Примітки</label>
+	          <Field
+	            type="text"
+	            name="note"
+	            validate={validateName1} 
+	            disabled={isSubmitting}
+	          />
+	          <ErrorMessage name='note'>	{msg => <div>{'ERROR: ' + msg}</div>} </ErrorMessage>
+
+
+
+
 	          <button type="submit" disabled={isSubmitting}>Submit</button>
 	          <Loader isLoading={isSubmitting}/>
 	        </Form>
